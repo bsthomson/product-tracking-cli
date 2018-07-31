9a361dbad807803f0b60869d46ab31349a193f1b
bsthomson <thomson.bs@gmail.com>
2018-07-31 18:30:48 -0500

    changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,8 +9,21 @@ var connection = mysql.createConnection({
     database : 'bamazon_db'
 });
 
+function showProduct() {
+    console.log("Here are our products!\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log("Id --- Product --- Price --- Quantity")
+      for (i=0; i < res.length; i++) {
+      console.log(res[i].id + " --- " + res[i].product_name + " --- $" + res[i].price + " --- " + res[i].stock_quantity);
+      }
+    connection.end();
+    });
+};
+
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    connection.end();
+    showProduct();
   });

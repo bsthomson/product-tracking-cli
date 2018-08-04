2b2ef8a28ff5e32ab3dd8174e32784731b259c81
bsthomson <thomson.bs@gmail.com>
2018-08-04 18:47:48 -0500

    changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,6 +9,10 @@ var connection = mysql.createConnection({
     database : 'bamazon_db'
 });
 
+var productId;
+var amount;
+var total;
+
 function showProduct() {
     console.log("Here are our products!\n");
     connection.query("SELECT * FROM products", function(err, res) {
@@ -44,19 +48,19 @@ function idFind(answer) {
     var query = "SELECT id FROM products";
     connection.query(query, function(err, res) {
         if (err) throw err;
-        var productId= parseInt(answer.idChoice);
+        productId= parseInt(answer.idChoice);
         var idArray = [];
         for (var i=0; i < res.length; i++) {
             idArray.push(res[i].id);
         };
         if (idArray.includes(productId)) {
-            return question2(productId);
+            return question2();
         } else {
             console.log("That id can not be found. Please choose again");                    
             return question1();
         };
     });
-}
+};
 
 function question2() {
     inquirer.prompt(
@@ -69,25 +73,41 @@ function question2() {
         sellProducts(answer);
     })
     
-}
+};
 
 function sellProducts(answer){
     var query = "SELECT id, price, stock_quantity FROM products WHERE ?";
     connection.query(query, { id: productId }, function(err, res) {
         if (err) throw err;
-        if (isNanN(parseInt(answer))) {
+        amount = parseInt(answer.quantityChoice);
+        total = res[0].stock_quantity;
+        if (isNaN(amount)) {
             console.log("choose a number please");
             question2();
-        } else if (parseInt(answer) > res.stock_quantity) {
+        } else if (amount > res[0].stock_quantity) {
             console.log("We don't have that much in stock sorry!")
             question2();
         } else {
-            var cost = parseInt(answer) * res.price;
+            var cost = amount * res[0].price;
             console.log("That'll be $" + cost + ".")
             removeProducts();
-        }
+        };
+    });
+};
 
-    })
-}
-
-function rem
+function removeProducts(){
+    var query = "UPDATE products SET ? WHERE ?";
+    connection.query(query,
+        [
+            {
+                stock_quantity: total -= amount 
+            },
+            {
+                id: productId
+            }
+        ],
+        function (err, res) {
+            console.log("Here's " + amount + " units!")
+            connection.end();
+        })
+};

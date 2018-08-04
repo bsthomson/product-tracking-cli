c711897b0a5bc902f9ca0fa64780430264257f06
bsthomson <thomson.bs@gmail.com>
2018-08-03 23:57:53 -0500

    changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,7 @@ function showProduct() {
       for (i=0; i < res.length; i++) {
       console.log(res[i].id + " --- " + res[i].product_name + " --- $" + res[i].price + " --- " + res[i].stock_quantity);
       }
-    connection.end();
+    question1();
     });
 };
 
@@ -27,3 +27,46 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId + "\n");
     showProduct();
   });
+
+function question1() {
+  inquirer.prompt(
+        {
+        type: "input",
+        name: "idChoice",
+        message: "What is the ID of the product you'd like to buy?"
+        }
+    ).then(function(answer){
+        var query = "SELECT id FROM products";
+        connection.query(query, function(err, res) {
+            if (err) throw err;
+            var idString;
+            var productId= answer.idChoice;
+            for (i=0; i < res.length; i++) {
+                idString =+ " " + res[i].id;
+                console.log(idString);
+                if (idString.indexOf(answer.idChoice) === -1) {
+                    console.log("That id can not be found. Please choose again");                    
+                    question1();
+                } else {
+                    return question2();
+                };
+            };
+        });
+    });
+};
+
+function question2() {
+    inquirer.prompt(
+        {
+        type: "input",
+        name: "quantityChoice",
+        message: "How much would you like to purchase?"
+        }
+    ).then(function(answer){
+        var query = "SELECT id, stock_quantity FROM products WHERE ?";
+        connection.query(query, { id: productId }, function(err, res) {
+
+        })
+    })
+    
+}
